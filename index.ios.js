1c11dcb013cdc5dc9d96f55c1c94971b1981e15e
pranavj1001 <pranavj1001@gmail.com>
2017-10-01 13:52:28 +0530

    add Header component

--- index.ios.js
+++ index.ios.js
@@ -5,10 +5,12 @@ import { Text, AppRegistry } from 'react-native';
 // Code to create a component
 // App Component
 // A Component is a JavaScript function that returns JSX
+// App Component is also our root component
 const App = () => (
   // this is JSX
   <Text>Hello World</Text>
 );
 
 // Render the components
+// use AppRegistry for root components only
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries to create components
+import React from 'react';
+import { Text } from 'react-native';
+
+// Code to create a component
+const Header = () => (
+  <Text>Albums!</Text>
+);
+
+// Make the component available to other parts of the app
+export default Header;
