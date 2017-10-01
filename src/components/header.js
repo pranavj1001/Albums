88d7540b7b74821409421dbfb13172c6eb7cf64a
pranavj1001 <pranavj1001@gmail.com>
2017-10-01 14:29:19 +0530

    stylize the header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,35 @@
 // Import libraries to create components
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Code to create a component
-const Header = () => (
-  <Text>Albums!</Text>
-);
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 30
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+};
 
 // Make the component available to other parts of the app
 export default Header;
