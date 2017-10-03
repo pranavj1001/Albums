b93766ad841bbf7a7f90c5432c04a9933ee74a4e
pranavj1001 <pranavj1001@gmail.com>
2017-10-04 01:55:42 +0530

    add albumArt

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,6 +1,6 @@
 // Import libraries to create components
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import AlbumImage from './AlbumImage';
@@ -10,18 +10,21 @@ const AlbumDetail = (props) => {
     return props.album.artists.map(artist => <Text key={artist.id}>{artist.name}</Text>);
   }
 
-  const { headerContentStyle, albumName } = styles;
+  const { headerContentStyle, albumName, thumbnailContainerStyle, albumArt } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <AlbumImage artist={props.album.artists[0].href} />
         </View>
         <View style={headerContentStyle}>
           <Text style={albumName}>{props.album.name}</Text>
           {renderArtistNames()}
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={albumArt} source={{ uri: props.album.images[1].url }} />
       </CardSection>
     </Card>
   );
@@ -34,6 +37,17 @@ const styles = {
   },
   albumName: {
     fontWeight: '700'
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumArt: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
--- src/components/AlbumImage.js
+++ src/components/AlbumImage.js
@@ -35,6 +35,6 @@ const styles = {
     width: 50,
     borderRadius: 25
   }
-}
+};
 
 export default AlbumImage;
